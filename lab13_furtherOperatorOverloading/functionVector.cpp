a006bacd7c2329736cd3134f819555593f547421
albusDalbador <lemiasheuski@student.agh.edu.pl>
2022-01-12 19:37:06 +0100

    lab 13 works properly

--- lab13_furtherOperatorOverloading/functionVector.cpp
+++ lab13_furtherOperatorOverloading/functionVector.cpp
@@ -1,5 +1,14 @@
 #include "functionVector.h"
 
+double FunctionVector::operator[](int num) const {
+    double result = 0.5;
+
+    for (int i = 0; i <= num; i++) {
+        result = functions[i](result);
+    }
+
+    return result;
+}
  
 void FunctionVector::insert(std::function<double(double)> func) {
     functions.push_back(func);
--- lab13_furtherOperatorOverloading/functionVector.h
+++ lab13_furtherOperatorOverloading/functionVector.h
@@ -3,13 +3,15 @@
 
 class FunctionVector {
 
+
     private:
         // std::vector<double(*)(double)> functions;
         std::vector<std::function<double(double)>> functions;
 
     public:
         // void inser
-
+        double operator[](int) const;
+        
         void insert(std::function<double(double)>);
         double calc(double);
 };
